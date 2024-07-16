3d7d2d217ebe3006f656c342a1e775b664df9972
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-16 14:53:15 -0300

    feat: add http server with post /nfe route

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { SNS } from "aws-sdk";
+import { z, ZodError } from "zod";
+
+const app = express();
+app.use(express.json());
+
+app.post("/nfe", async (request, response) => {
+  const requestBodySchema = z.object({
+    url: z.string().url(),
+  });
+
+  try {
+    const { url } = requestBodySchema.parse(request.body);
+    const sns = new SNS({
+      region: "us-east-1",
+    });
+
+    const { MessageId } = await sns
+      .publish({
+        Message: JSON.stringify({ url }),
+        TopicArn: process.env.AWS_TOPIC_ARN,
+        MessageGroupId: "01",
+      })
+      .promise();
+
+    if (!MessageId) {
+      throw Error("Fail to publish SNS message");
+    }
+
+    return response.status(201).json({ success: true });
+  } catch (error: unknown) {
+    console.log(error);
+
+    if (error instanceof ZodError) {
+      return response.status(400).json({
+        message: error.format(),
+      });
+    }
+    return response.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.listen(Number(process.env.PORT), () => {
+  console.log(`HTTP Server is running on port ${process.env.PORT}`);
+});
